67b2967f14624a90959e82dc3d02daff27d7f630
Oliver Levay <oliver.levay@hotmail.com>
2023-01-24 17:42:30 +0100

    add first test

--- dag4/backend/tests/medals.spec.ts
+++ dag4/backend/tests/medals.spec.ts
@@ -1,9 +1,22 @@
 import 'mocha';
 import chai, { expect } from 'chai';
 import spies from 'chai-spies';
+import createApolloServer from '../src/createApolloServer';
 
 describe('Medals', () => {
-  it('should be a test', () => {
-    expect(true).to.be.true;
+  const server = createApolloServer();
+  it('should be a test', async () => {
+    const response = await server.executeOperation({
+      query: `
+      query {
+        medals {
+          name
+        }
+      }
+      `,
+    });
+    if (response.body.kind === 'single') {
+      expect(response.body.singleResult.errors).to.be.undefined;
+    }
   });
 });
